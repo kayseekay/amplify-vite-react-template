3c1f4aca65be8f88498cea9a789173aa4cc4b598
Kevin Kessler <kevin@kesslerautomation.com>
2024-12-02 19:28:01 -0500

    chore(code): tidied imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import '@aws-amplify/ui-react/styles.css';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from "aws-amplify";
+import App from "./App.tsx";
 import outputs from "../amplify_outputs.json";
+import "./index.css";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
